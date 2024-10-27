94427eb60752fa0fd89b28520122af83de85193a
KONDO <kondon.94.j@gmail.com>
2024-10-27 19:12:17 +0900

    最初の画面出力確認

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/todo.blade.php -->
+<html>
+<body>
+    <h1>Todo App</h1>
+    <p>これはLaravelの最初のアプリです。</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/hello', function(){
     return 'Hello Laravel';
 });
+
+Route::get('/todo', [TodoController::class,'index']);
